3ab50fb2f7261dc396451d8790aef4132293d01e
bet-gregori <elisabet.gregori@gmail.com>
2017-11-30 15:00:46 +0100

    Fixed some incorrect calls to addEmptySubstance

--- compoundDB/inputtools.py
+++ compoundDB/inputtools.py
@@ -8,7 +8,15 @@ from rdkit import Chem
 from rdkit.Chem.Descriptors import MolWt
 from rdkit.Chem.Crippen import MolLogP
 
-def openconnection(host='gea', dbname='compounds', user='postgres', password='DBAdmin'):
+def openconnection(host='gea', dbname='compounds', user='postgres', password=''):
+    """
+        Opens a psycopg2 connection to the PostgreSQL DB and returns the connection object.
+        Arguments:
+          - host
+          - dbname
+          - user
+          - password
+    """
     conn = psycopg2.connect(host=host, dbname=dbname, user=user, password=password)
     curs = conn.cursor()
     curs.execute('create extension if not exists rdkit;')
@@ -16,6 +24,13 @@ def openconnection(host='gea', dbname='compounds', user='postgres', password='DB
     return conn
 
 def addSynonyms(conn, subsID, synD):
+    """
+        Add all synonyms provided for a given substance.
+        Arguments:
+          - conn: psycopg2 connection to the database.
+          - subsID: id for the partent subsance from the 'substance' table.
+          - synD: Dictionary with the substance synonyms' and their types {type: synonym}.
+    """
     curs = conn.cursor()
     cmd = "INSERT INTO synonym (subsid, type, name) \
             VALUES (%s, %s, %s)"
@@ -24,6 +39,18 @@ def addSynonyms(conn, subsID, synD):
 
 def addSource(conn, name, version= None, description= None, link= None, \
               date= datetime.datetime.now().date()):
+    """
+        Insert a source to the DB.
+        Arguments:
+          - conn: psycopg2 connection to the database.
+          - name: Source name.
+          - version: Optional. Source's version (default: None). If None, if another source with the same name exists the version will be increased by one, else the version will be 1.
+          - description: Optional. Verbose description of the source (default: None).
+          - link: Optional. Link to the source's home page (default: None).
+          - date: Optional. Date of source's creation (default: Today).
+
+        Returns source id from the 'source' table.
+    """
     curs = conn.cursor()
     # Check if version is provided, otherwise generate it
     if version is None:
@@ -56,6 +83,16 @@ def addSource(conn, name, version= None, description= None, link= None, \
     return sourceID
 
 def addEmptyCompound(conn, subsID, smiles):
+    """
+        Insert an empty compound to the DB, in case a compound's smiles is available but it can't be converted into an RDkit mol object.
+        Also inserts an instance to the 'subs_cmpd' table with the parent's substance id and the compound id.
+        Arguments:
+          - conn: psycopg2 connection to the database.
+          - subsID: id for the partent subsance from the 'substance' table.
+          - smiles: Smiles string for the compound.
+
+        Returns compound id from the 'compound' table.
+    """
     curs = conn.cursor()
     cmd = "SELECT id FROM compound WHERE smiles = %s;"
     curs.execute(cmd, (smiles,))
@@ -80,22 +117,34 @@ def addEmptyCompound(conn, subsID, smiles):
     curs.execute(cmd, (subsID, cmpdID))
     conn.commit()
     
-    return (cmpdID, None)
+    return cmpdID
 
-def addCompound(conn, sourceID, subsID, smiles= None, mol= None, ismetal=False):
+def addCompound(conn, subsID, smiles= None, mol= None, ismetal=False):
+    """
+        Insert one standardised compound to the DB. Must provide either a smiles string or an RDKit mol object.
+        Also inserts an instance to the 'subs_cmpd' table with the parent's substance id and the compound id.
+        Arguments:
+          - conn: psycopg2 connection to the database.
+          - subsID: id for the partent subsance from the 'substance' table.
+          - smiles: Optional. Smiles string for the compound (default: None).
+          - mol: Optional. RDkit mol object (default: None).
+          - ismetal: Optional. Boolean flag indicating whether the compound is a metal ion (default: False).
+
+        Returns compound id from the 'compound' table.
+    """
     curs = conn.cursor()
     if smiles is None and mol is None:
-        (cmpdID, mol) = addEmptyCompound(conn, subsID, smiles)
+        cmpdID = addEmptyCompound(conn, subsID, smiles)
     elif mol is None:
         try:
             mol = Chem.MolFromSmiles(smiles)
         except:
             print (smiles)
-            (cmpdID, mol) = addEmptyCompound(conn, subsID, smiles)
+            cmpdID = addEmptyCompound(conn, subsID, smiles)
             
     if mol is None:
         # Molecule object has been created, but only as an empty instnce
-        (cmpdID, mol) = addEmptyCompound(conn, subsID, smiles)            
+        cmpdID = addEmptyCompound(conn, subsID, smiles)            
     else:
         inchi = Chem.MolToInchi(mol)
         inchikey = Chem.InchiToInchiKey(inchi)
@@ -131,9 +180,19 @@ def addCompound(conn, sourceID, subsID, smiles= None, mol= None, ismetal=False):
     curs.execute(cmd, (subsID, cmpdID))
     conn.commit()
     
-    return (cmpdID, mol)
+    return cmpdID
 
-def addEmptySubstance(conn, sourceID, extID, molID= None, link= None):
+def addEmptySubstance(conn, sourceID, extID, link= None):
+    """
+        Insert an empty substance to the DB, in case a substance is present in the source but it doesn't have a structure or it can't be converted into an RDkit mol object.
+        Arguments:
+          - conn: psycopg2 connection to the database.
+          - sourceID: id for the source of origin from the 'source' table.
+          - extID: id for the subsance in the source of origin.
+          - link: Optional. Link to information on the substance (default: None).
+
+        Returns a tupple with the substance id from the 'substance' table and None, since no RDKit mol object is available.
+    """
     curs = conn.cursor()
     cmd = "SELECT id FROM substance WHERE sourceid = %s \
             AND externalid = %s;"
@@ -157,18 +216,30 @@ def addEmptySubstance(conn, sourceID, extID, molID= None, link= None):
     return (subsID, None)
 
 def addSubstance(conn, sourceID, extID, smiles= None, mol= None, link= None):
+    """
+        Standardise and insert a substance to the DB. Must provide either a smiles string or an RDKit mol object.
+        Arguments:
+          - conn: psycopg2 connection to the database.
+          - sourceID: id for the source of origin from the 'source' table.
+          - extID: id for the subsance in the source of origin.
+          - smiles: Optional. Smiles string for the substance (default: None).
+          - mol: Optional. RDkit mol object (default: None).
+          - link: Optional. Link to information on the substance (default: None).
+
+        Returns a tupple with the substance id from the 'substance' table and the RDKit mol object of the standardised substance if available.
+    """
     curs = conn.cursor()
     if smiles is None and mol is None:
-        (subsID, mol) = addEmptySubstance(curs, conn, sourceID, extID, link)
+        (subsID, mol) = addEmptySubstance(conn, sourceID, extID, link)
     elif mol is None:
         try:
             mol = Chem.MolFromSmiles(smiles)
         except:
-            (subsID, mol) = addEmptySubstance(curs, conn, sourceID, extID, link)
+            (subsID, mol) = addEmptySubstance(conn, sourceID, extID, link)
             
     if mol is None:
         # Molecule object has been created, but only as an empty instnce
-        (subsID, mol) = addEmptySubstance(curs, conn, sourceID, extID, link)
+        (subsID, mol) = addEmptySubstance(conn, sourceID, extID, link)
     else:
         cmd = "SELECT id FROM substance WHERE sourceid = %s \
                 AND externalid = %s;"
@@ -194,13 +265,28 @@ def addSubstance(conn, sourceID, extID, smiles= None, mol= None, link= None):
             stdD = ps.std(mol)
             for smiles in stdD:
                 (cmpd, ismetal) = stdD[smiles]
-                (cmpdID, cmpdmol) = addCompound(conn, sourceID, subsID, smiles= smiles, mol= cmpd, ismetal=ismetal)
+                cmpdID = addCompound(conn, subsID, smiles= smiles, mol= cmpd, ismetal=ismetal)
         else:
             subsID = subsID[0]
             
     return (subsID, mol)
 
-def addSubstanceFile(conn, sourceID, fname, ftype, molID= None, smilesI= 1, linkI= None, header= False, synonyms= None):
+def addSubstanceFile(conn, sourceID, fname, ftype, extID= None, smilesI= 1, linkI= None, header= False, synonyms= None):
+    """
+        Process a file with substances from a given source. The file can either be in SD format or a text file with smiles strings.
+        Arguments:
+          - conn: psycopg2 connection to the database.
+          - sourceID: id for the source of origin from the 'source' table.
+          - fname: Input file name.
+          - ftype: Input file type (sdf / smi).
+          - extID: Field containing the id of the substance in the source of origin (default: None). 
+            - If the input format is 'sdf', it can be a field name or None. If None, the substance name from the first line will be used and if this is empty an id will be generated with a substance counter. 
+            - If the input format is 'smi', it can either be an integer corresponding to the column index or the name of the header of the column containing the substance id. If None, an id will be generated with a substance counter.
+          - smilesI: Optional. Field containing substance's smiles string. It can either be an integer corresponding to the column index or the name of the header of the column containing the smiles string (default: 1).
+          - linkI: Optional. Field containing a link to the substance information page (default: None). 
+            - If the input format is 'sdf', it can be a field name or None.  
+            - If the input format is 'smi', it can either be an integer corresponding to the column index or the name of the header of the column containing the link. 
+    """
     curs = conn.cursor()
     if ftype == 'smi':                
         with open(fname) as f:
@@ -216,8 +302,8 @@ def addSubstanceFile(conn, sourceID, fname, ftype, molID= None, smilesI= 1, link
                         synTypes = list(synonyms)
                         for t in synTypes:
                             synIndices.append(header.index(t))
-                if not isinstance(molID, int):
-                    molID = header.index(molID)
+                if not isinstance(extID, int):
+                    extID = header.index(extID)
                 if not isinstance(smilesI, int):
                     smilesI = header.index(smilesI)
             molcount = 0
@@ -225,10 +311,10 @@ def addSubstanceFile(conn, sourceID, fname, ftype, molID= None, smilesI= 1, link
                 molcount += 1
                 fields = line.rstrip().split('\t')
                 smi = fields[smilesI]
-                if molID is None:
+                if extID is None:
                     extID = 'mol%0.8d'%molcount
                 else:
-                    extID = fields[molID]
+                    extID = fields[extID]
                 if linkI is None: link = None
                 else: link= fields[linkI]
 
@@ -257,11 +343,11 @@ def addSubstanceFile(conn, sourceID, fname, ftype, molID= None, smilesI= 1, link
         for mol in suppl:
             molcount += 1
             if mol is None:
-                extID = mh.getNameFromEmpty(suppl, molcount-1, molID)
+                extID = mh.getNameFromEmpty(suppl, molcount-1, extID)
                 (subsID, mol) = addEmptySubstance(conn, sourceID, extID, link)
                 
             else:                
-                extID = mh.getName(mol, molcount, molID)                    
+                extID = mh.getName(mol, molcount, extID)                    
                 link = None
                 if linkI is not None:
                     try:
@@ -285,8 +371,25 @@ def addSubstanceFile(conn, sourceID, fname, ftype, molID= None, smilesI= 1, link
             addSynonyms(conn, subsID, synD)
 
 def addSubstanceFromQuery(conn, sourceID, cmd, host='gea', dbname='chembl_23', user='postgres', \
-                          password='DBAdmin', extIDf= 'ID', smilesF= 'smiles', \
+                          password='', extIDf= 'ID', smilesF= 'smiles', \
                           linkF= None):
+    """
+        Add substances from a given source from a PostgeSQL DB. 
+        Arguments:
+          - conn: psycopg2 connection to the database where the substances will be input.
+          - sourceID: id for the source of origin from the 'source' table.
+          - cmd: Postgres statement to extract the substances.
+          Connection details for the DB containing the substances:
+            - host: 
+            - dbname
+            - user
+            - password
+          - fname: Input file name.
+          - ftype: Input file type (sdf / smi).
+          - extIDf: Name of the DB field containing the id of the substance in the source of origin (default: ID). 
+          - smilesF: Name of the DB field containing the smiles string (default: smiles). 
+          - linkF: Optional. Name of the DB field containing the link to the substance information page (default: None). 
+    """
     
     # Open connection to the query DB
     qconn = psycopg2.connect(host=host, dbname=dbname, user=user, password=password)
