1a66934953370c3cf28c18d5fa87e63b76a89c7c
bet-gregori <elisabet.gregori@gmail.com>
2018-01-22 12:45:18 +0100

    Added tools and data to manage annotations DB

---
+++ annotationDB/inputtools.py
@@ -0,0 +1,398 @@
+import sys, os, datetime, pickle
+from compoundDB import querytools as qt
+
+import psycopg2
+
+annClassification_file = "annD.pkl"
+annC_fh = open(os.path.join(os.path.dirname(__file__), "data",  annClassification_file), 'rb')
+annD = pickle.load(annC_fh)
+annC_fh.close()
+
+def openconnection(host='gea', dbname='annotations', user='postgres', password=''):
+    """
+        Opens a psycopg2 connection to the PostgreSQL DB and returns the connection object.
+        Arguments:
+          - host
+          - dbname
+          - user
+          - password
+    """
+    conn = psycopg2.connect(host=host, dbname=dbname, user=user, password=password)
+
+    return conn
+
+def standardiseAnnotation(ann):
+    """
+    """
+    d = annD[ann]
+    return d
+
+def addAnnotation(conn, subsID, ann, annType=None, annCategory=None, generalAnn=None):
+    """
+    """
+    ann = ann.strip().strip('\"').strip()
+    try:
+        d = standardiseAnnotation(ann)
+    except:
+        std_ann = ann
+        if not annType:
+            annType = 'Confirmed'
+        annCategory = ''
+        generalAnn = ''
+    else:
+        std_ann = d['Corrected annotation']
+        if not annType:
+            annType = 'Confirmed'
+        if not annCategory:
+            annCategory = d['Category']
+        if not generalAnn:
+            generalAnn = d['GeneralAnnotation']
+    curs = conn.cursor()
+
+    cmd = "SELECT id FROM annotation WHERE annotation = %s;"
+    curs.execute(cmd, (std_ann,))
+    annID = curs.fetchone()
+    conn.commit()
+    
+    if annID is None:
+        cmd = "INSERT INTO annotation (annotation, general, category)\
+               VALUES (%s, %s, %s)"
+        curs.execute(cmd, (std_ann, generalAnn, annCategory))
+        conn.commit()
+        cmd = "SELECT currval('annotation_id_seq');"
+        curs.execute(cmd)
+        annID = curs.fetchone()[0]
+        conn.commit()
+    else:
+        annID = annID[0]
+
+    cmd = "INSERT INTO subs_ann (subsid, annid, original_annotation, type)\
+           SELECT %s, %s, %s, %s \
+           WHERE NOT EXISTS (SELECT subsid, annid, original_annotation, type \
+               FROM subs_ann \
+               WHERE subsid= %s AND annid= %s)"
+    curs.execute(cmd, (subsID, annID, ann, annType, subsID, annID))
+    conn.commit()
+
+def addSynonyms(conn, subsID, synD):
+    """
+        Add all synonyms provided for a given substance.
+        Arguments:
+          - conn: psycopg2 connection to the database.
+          - subsID: id for the partent subsance from the 'substance' table.
+          - synD: Dictionary with the substance synonyms' and their types {type: synonym}.
+    """
+    curs = conn.cursor()
+    cmd = "INSERT INTO public.synonym (subsid, type, name)\
+           SELECT %s, %s, %s\
+           WHERE NOT EXISTS (SELECT subsid, type, name\
+               FROM public.synonym \
+               WHERE subsid= %s AND type= %s \
+               AND name= %s)"
+    curs.executemany(cmd, [(subsID,syntype,syn,subsID,syntype,syn) for syntype in synD for syn in synD[syntype]])
+    conn.commit()
+
+def addSource(conn, sourceName, version= None, description= None, link= None, \
+              date= datetime.datetime.now().date()):
+    """
+        Insert a source to the DB.
+        Arguments:
+          - conn: psycopg2 connection to the database.
+          - sourceName: Source name.
+          - version: Optional. Source's version (default: None). If None, if another source with the same name exists the version will be increased by one, else the version will be 1.
+          - description: Optional. Verbose description of the source (default: None).
+          - link: Optional. Link to the source's home page (default: None).
+          - date: Optional. Date of source's creation (default: Today).
+
+        Returns source id from the 'source' table.
+    """
+    curs = conn.cursor()
+    # Check if version is provided, otherwise generate it
+    if not version:
+        cmd = "SELECT id FROM source WHERE name = %s ORDER BY version DESC;"
+        curs.execute(cmd, (sourceName,))
+        oldVersion = curs.fetchone()
+        conn.commit()
+        if oldVersion is None:
+            version = '1'
+        else:
+            version += str(int(oldVersion[0]))
+            
+    cmd = "SELECT id FROM source WHERE name = %s AND version = %s;"
+    curs.execute(cmd, (sourceName, str(version)))
+    sourceID = curs.fetchone()
+    conn.commit()
+    
+    if sourceID is None:
+        cmd = "INSERT INTO source (name, version, description, link, added)\
+               VALUES (%s, %s, %s, %s, %s)"
+        curs.execute(cmd, (sourceName, version, description, link, date))
+        conn.commit()
+        cmd = "SELECT currval('source_id_seq');"
+        curs.execute(cmd)
+        sourceID = curs.fetchone()[0]
+        conn.commit()
+    else:
+        sourceID = sourceID[0]
+    
+    return sourceID
+
+def addSubstance(conn, sourceID, extID, link= None):
+    """
+        Insert a substance to the DB. 
+        Arguments:
+          - conn: psycopg2 connection to the database.
+          - sourceID: id for the source of origin from the 'source' table.
+          - extID: id for the subsance in the source of origin.
+          - link: Optional. Link to information on the substance (default: None).
+
+        Returns the substance id from the 'substance' table.
+    """
+    curs = conn.cursor()
+            
+    cmd = "SELECT id FROM substance WHERE sourceid = %s \
+            AND externalid = %s;"
+    curs.execute(cmd, (sourceID, extID))
+    subsID = curs.fetchone()
+    conn.commit()
+
+    if not subsID:
+        cmd = "INSERT INTO substance (sourceid, externalid, link) \
+                VALUES (%s, %s, %s)"
+        curs.execute(cmd, (sourceID, extID, link))
+        conn.commit()
+        cmd = "SELECT currval('substance_id_seq');"
+        curs.execute(cmd)
+        subsID = curs.fetchone()[0]
+        conn.commit()
+        addSynonyms(conn, subsID, {'ExternalID': extID})
+    else:
+        subsID = subsID[0]
+            
+    return subsID
+
+def addSubstancesFromFile(conn, sourceID, fname, extIDindex= None, extIDfield= None, 
+                        annIndex= 1, annField= 'smiles', annType = None, typeIndex= None, typeField= None, linkIndex= None, linkField= None, synonymsIndices= None, synonymsFields= None, header= True):
+    """
+        Process a text file with substances and annoations from a given source.
+        Arguments:
+          - conn: psycopg2 connection to the database.
+          - sourceID: id for the source of origin from the 'source' table.
+          - fname: Input file name.
+          - extIDindex: Optional. Index of the column containing the id of the substance in the source of origin (default: None). If None, an id will be generated with a substance counter.
+          - extIDfield: Optional. Name of the header of the column containing the substance id (default: None). If None, an id will be generated with a substance counter.
+          - annIndex: Optional. Index of the column containing the substance's annotation (default: 1). 
+          - annField: Optional. Name of the header of the column containing the substance's annotation (default: 'Annotation').
+          - annType: Optional. Type of all the annotations of the substances for this source (default: None). It should be one of the following: 'Confirmed', 'Suspected', 'Negative'.
+          - typeIndex: Optional. Index of the column containing the type of the annotation in the source of origin (default: None). It should be one of the following: 'Confirmed', 'Suspected', 'Negative'.
+          - typeField: Optional. Name of the header of the column containing the type of the annotation (default: None). It should be one of the following: 'Confirmed', 'Suspected', 'Negative'.
+          - linkIndex: Optional. Index of the column containing a link to the substance information page (default: None). 
+          - linkField: Optional. Name of the header of the column containing a link to the substance information page (default: None).
+          - synonymsIndices: Optional. List of indices of the column(s) containing synonyms of the substance (default: None). Synonym type will be 'Name'.
+          - synonymsFields: Optional. List of name(s) of the header of the column(s) containing synonyms of the substance (default: None). 
+          - header: Boolean indicating if the file has a header (default: False).
+    """
+    curs = conn.cursor()          
+    with open(fname) as f:
+        if header: 
+            header = f.readline().rstrip().split('\t')
+            if extIDfield:
+                extIDindex = header.index(extIDfield)
+
+            if annField:
+                annIndex = header.index(annField)
+
+            if typeField:
+                typeIndex = header.index(typeField)
+
+            if linkField:
+                linkIndex = header.index(linkField)
+
+            if synonymsFields:
+                synTypes = synonymsFields
+                synonymsIndices = []
+                for t in synTypes:
+                    synonymsIndices.append(header.index(t))
+
+            elif synonymsIndices:
+                synTypes = []
+                for i in synonymsIndices:
+                    synTypes.append(header[i].strip())
+
+        else:
+            # If the file has no header and some columns contain synonyms, 
+            # set each synonym type to 'Name'
+            if synonymsIndices:
+                synonymsIndices = synonymsIndices
+                synTypes = []
+                for i in synonymsIndices:
+                    synTypes.append('Name')
+
+        molcount = 0
+        for line in f:
+            molcount += 1
+            fields = line.rstrip().split('\t')
+            try:
+                ann = fields[annIndex]
+            except:
+                continue
+            else:
+                if ann == '****':
+                    continue
+
+            if extIDindex is None:
+                # No field with the ID of the substance in the source of origin 
+                # has been provided so one will be generated.
+                extID = 'mol%0.8d'%molcount
+            else:
+                try:
+                    extID = fields[extIDindex]
+                except:
+                    continue
+
+            if not linkIndex: link = None
+            else: link= fields[linkIndex]
+
+            # Add the subsance
+            subsID = addSubstance(conn, sourceID, extID= extID, link= link)
+
+            # Add the annotation
+            if typeIndex is not None:
+                try:
+                    ann_type = fields[typeIndex]
+                except:
+                    ann_type = None
+            else:
+                ann_type = annType
+            addAnnotation(conn, subsID, ann, ann_type)
+                
+            # Add synonyms
+            synD = {}
+            if synonymsIndices:
+                for i in range(len(synonymsIndices)):
+                    sindex = synonymsIndices[i]
+                    stype = synTypes[i]
+                    if len(fields) <= sindex: continue
+                    syn = fields[sindex].strip()
+                    if syn in ('N/A', '-', '', '--', '---'): continue
+                    if stype not in synD:
+                        synD[stype] = set([syn])
+                    else:
+                        synD.add(syn)
+            addSynonyms(conn, subsID, synD)
+
+def addSubstanceFromQuery(conn, sourceID, cmd, host='gea', dbname='compounds', user='postgres',
+                          password='', extIDf= 'ID', linkF= None):
+    """
+        Add substances from a given source from a PostgeSQL DB. 
+        Arguments:
+          - conn: psycopg2 connection to the database where the substances will be input.
+          - sourceID: id for the source of origin from the 'source' table.
+          - cmd: Postgres statement to extract the substances.
+          Connection details for the DB containing the substances:
+            - host: 
+            - dbname
+            - user
+            - password
+          - fname: Input file name.
+          - ftype: Input file type (sdf / smi).
+          - extIDf: Name of the DB field containing the id of the substance in the source of origin (default: ID).
+          - linkF: Optional. Name of the DB field containing the link to the substance information page (default: None). 
+    """
+    
+    # Open connection to the query DB
+    qconn = psycopg2.connect(host=host, dbname=dbname, user=user, password=password)
+    df = pd.read_sql(cmd, con=qconn)
+    for index, row in df.iterrows():
+        extID = row[extIDf]
+        if not linkF: link = None
+        else: link= row[linkF]
+        try:
+            mol = Chem.MolFromSmiles(smi)
+        except:
+            addEmptySubstance(conn, sourceID, extID, link)
+            continue
+        addSubstance(conn, sourceID, extID= extID, mol= mol, link= link)
+
+def addSynonymsFromFile(conn, fname, sourceID= None, sourceName= None, version= None,extIDindex= None, extIDfield= None, synonymsIndices= None, synonymsFields= None, header= False):
+    """
+    Add synonyms for substances already in the DB.
+    Arguments:
+        - conn: psycopg2 connection to the database.
+        - fname: Input file name.
+        - sourceID: Optional. Internal source ID if avaialble. Otherwise, it will be retrieved from he source name and version.
+        - sourceName: Source name.
+        - version: Optional. Source's version (default: None). If None, 
+        the last version will be used.
+        - extIDindex: Optional. Index of the column containing the id of the substance in the source of origin (default: None). If None, an id will be generated with a substance counter.
+        - extIDfield: Optional. Name of the header of the column containing the substance id (default: None). If None, an id will be generated with a substance counter.
+        - synonymsIndices: Optional. List of indices of the column(s) containing synonyms of the substance (default: None). Synonym type will be 'Name'.
+        - synonymsFields: Optional. List of name(s) of the header of the column(s) containing synonyms of the substance (default: None). 
+        - header: Boolean indicating if the file has a header (default: False).
+    """
+    curs = conn.cursor()
+    if sourceID is None:
+        sourceID = getSourceID(conn, sourceName, version)
+    with open(fname) as f:
+        if header: 
+            header = f.readline().rstrip().split('\t')
+            if extIDfield:
+                extIDindex = header.index(extIDfield)
+
+            if synonymsFields:
+                synTypes = synonymsFields
+                synonymsIndices = []
+                for t in synTypes:
+                    synonymsIndices.append(header.index(t))
+
+            elif synonymsIndices:
+                synTypes = []
+                for i in synonymsIndices:
+                    synTypes.append(header[i].strip())
+
+        else:
+            # If the file has no header and some columns contain synonyms, 
+            # set each synonym type to 'Name'
+            if synonymsIndices:
+                synonymsIndices = synonymsIndices
+                synTypes = []
+                for i in synonymsIndices:
+                    synTypes.append('Name')
+
+        molcount = 0
+        for line in f:
+            molcount += 1
+            fields = line.rstrip().split('\t')
+            if extIDindex is None:
+                # No field with the ID of the substance in the source of origin 
+                # has been provided so one will be generated.
+                extID = 'mol%0.8d'%molcount
+            else:
+                try:
+                    extID = fields[extIDindex]
+                except:
+                    if smi is None:
+                        continue
+                    else:
+                        extID = 'mol%0.8d'%molcount
+
+            # Get substance ID
+            subsID = qt.getSubsID(conn, sourceID, extID)
+            if not subsID:
+                # The substance is not in the DB
+                continue
+                
+            # Add synonyms
+            synD = {'ExternalID': set([extID])}
+            if synonymsIndices:
+                for i in range(len(synonymsIndices)):
+                    sindex = synonymsIndices[i]
+                    stype = synTypes[i]
+                    if len(fields) <= sindex: continue
+                    syn = fields[sindex].strip()
+                    if syn == 'N/A' or syn == '': continue
+                    if stype not in synD:
+                        synD[stype] = set([syn])
+                    else:
+                        synD.add(syn)
+            addSynonyms(conn, subsID, synD)
---
+++ annotationDB/querytools.py
@@ -0,0 +1,290 @@
+import sys, datetime
+
+import psycopg2
+import pandas as pd
+
+def getSubsID(conn, sourceID, extID):
+    """
+    Return the ID for a substance given the source and external ID.
+    Arguments:
+          - conn: psycopg2 connection to the database.
+          - sourceID: id for the source of origin from the 'source' table.
+          - extID: id for the subsance in the source of origin.
+    """
+    curs = conn.cursor()
+    cmd = "SELECT id FROM substance WHERE sourceid = %s AND externalid = %s;"
+    curs.execute(cmd, (sourceID, extID))
+    subsID = curs.fetchone()
+    conn.commit()
+
+    if subsID is not None:
+        subsID = subsID[0]
+
+    return subsID
+
+def getSourceID(conn, sourceName, version= None):
+    """
+    Return the ID for a source given the source name and optionally the version. 
+    If no version is provided, the last version will be used.
+    Arguments:
+          - conn: psycopg2 connection to the database.
+          - sourceName: Source name.
+          - version: Optional. Source's version (default: None). If None, 
+          the last version will be used.
+    """
+    curs = conn.cursor()
+    if version is None:
+        # Get the last version        
+        cmd = "SELECT id FROM source WHERE name = %s ORDER BY version DESC LIMIT 1;"
+        curs.execute(cmd, (sourceName,))
+    else:
+        # Get a specific version
+        cmd = "SELECT id FROM source WHERE name = %s AND version = %s;"
+        curs.execute(cmd, (sourceName, version))
+
+    sourceID = curs.fetchone()[0]
+    conn.commit()
+
+    return sourceID
+
+def getAllSourcesWithInfo(conn):
+    """
+    Return a pandas dataframe with all sources and the number of substances and compounds
+    by version.
+    """
+    cmd = "SELECT name, version, \
+                  COUNT(DISTINCT subs.externalid) AS entries, \
+                  COUNT(ann.annotation) AS all_annotations \
+                  COUNT(DISTINCT ann.annotation) AS unique_annotations \
+	       FROM public.source as s \
+           INNER JOIN public.substance AS subs ON s.id = subs.sourceid\
+	       LEFT OUTER JOIN public.subs_ann AS sa ON sa.subsid = subs.id \
+	       LEFT OUTER JOIN public.annotation AS ann ON sa.annid = ann.id \
+	       GROUP BY name, version"
+    df = pd.read_sql(cmd, con=conn)
+
+    return df
+
+def getAllSourcesWithInfoLimitSubstances(conn, ids):
+    """
+    Return a pandas dataframe with all sources and the number of substances and compounds
+    by version.
+    Arguments:
+          - conn: psycopg2 connection to the database.
+          - ids: Tuple with external IDs of compounds to consider.
+    """
+    cmd = "SELECT name, version, \
+                  COUNT(DISTINCT subs.externalid) AS entries, \
+                  COUNT(ann.annotation) AS all_annotations \
+                  COUNT(DISTINCT ann.annotation) AS unique_annotations \
+	       FROM public.source as s \
+           INNER JOIN public.substance AS subs ON s.id = subs.sourceid\
+	       LEFT OUTER JOIN public.subs_ann AS sa ON sa.subsid = subs.id \
+	       LEFT OUTER JOIN public.annotation AS ann ON sa.annid = ann.id \
+           WHERE subs.externalid in %s \
+	       GROUP BY name, version"
+    df = pd.read_sql(cmd, con=conn, params= (ids,))
+
+    return df
+
+def getOneSourceWithInfo(conn, sourceID= None, sourceName= None, version= None):
+    """
+    Return a pandas dataframe with all sources and the number of substances and compounds
+    by version.
+    Arguments:
+        - conn: psycopg2 connection to the database.
+        - sourceID: Optional. Internal source ID if avaialble. Otherwise, it will be retrieved    from the source name and version.
+        - sourceName: Optional. Source name.
+        - version: Optional. Source's version (default: None). If None, 
+          the last version will be used.
+    """
+    if sourceID is None:
+        sourceID = getSourceID(conn, sourceName, version)
+
+    cmd = "SELECT name, version, \
+                  COUNT(DISTINCT subs.externalid) AS entries, \
+                  COUNT(ann.annotation) AS all_annotations \
+                  COUNT(DISTINCT ann.annotation) AS unique_annotations \
+	       FROM public.source as s \
+           INNER JOIN public.substance AS subs ON s.id = subs.sourceid\
+	       LEFT OUTER JOIN public.subs_ann AS sa ON sa.subsid = subs.id \
+	       LEFT OUTER JOIN public.annotation AS ann ON sa.annid = ann.id \
+           WHERE s.id = %s \
+	       GROUP BY name, version"
+    df = pd.read_sql(cmd, con=conn, params=(sourceID,))
+
+    return df
+
+# def getMulipleSourcesWithInfo(conn, sourceID= None, sourceName= None, version= None):
+#     """
+#     Return a pandas dataframe with all sources and the number of substances and compounds
+#     by version.
+#     Arguments:
+#         - conn: psycopg2 connection to the database.
+#         - sourceID: Optional. List of internal source IDs if avaialble. Otherwise, they will be retrieved from the source name and version.
+#         - sourceName: Optional. List of source names.
+#         - version: Optional. List of source versions (default: None). If None, 
+#           the last version will be used.
+#     """
+#     if sourceID is None:
+#         sourceID = getSourceID(conn, sourceName, version)
+
+#     cmd = "SELECT name, version, \
+#                   COUNT(DISTINCT subs.externalid) AS entries, \
+#                   COUNT(DISTINCT subs.externalid) FILTER (WHERE subs.smiles IS NOT NULL) AS substances, \
+#                   COUNT(DISTINCT cmpd.inchikey) AS compounds \
+# 	       FROM public.source as s \
+#            INNER JOIN public.substance AS subs ON s.id = subs.sourceid\
+# 	       LEFT OUTER JOIN public.subs_cmpd AS sc ON sc.subsid = subs.id \
+# 	       LEFT OUTER JOIN public.compound AS cmpd ON sc.cmpdid = cmpd.id \
+# 	       GROUP BY name, version"
+#     df = pd.read_sql(cmd, con=conn)
+
+#     return df
+
+def getSubstancesFromSource(conn, sourceID= None, sourceName= None, version= None):
+    """
+    Retruns a pandas dataframe with all substances for the given source.
+    The dataframe contains:
+        - exernalid
+        - link
+    Arguments:
+        - conn: psycopg2 connection to the database.
+        - sourceID: Optional. Internal source ID if avaialble. Otherwise, it will be retrieved    from the source name and version.
+        - sourceName: Optional. Source name.
+        - version: Optional. Source's version (default: None). If None, 
+          the last version will be used.
+        without a chemical structure.
+    """
+    if sourceID is None:
+        sourceID = getSourceID(conn, sourceName, version)
+
+    if includeempty:
+        cmd = "SELECT externalid, subs.link \
+                FROM public.substance AS subs \
+                INNER JOIN public.source AS s ON s.id = subs.sourceid \
+                WHERE s.id = %s"
+    else:
+        cmd = "SELECT externalid, subs.link \
+                FROM public.substance AS subs \
+                INNER JOIN public.source AS s ON s.id = subs.sourceid \
+                WHERE s.id = %s AND smiles IS NOT NULL"
+
+    df = pd.read_sql(cmd, con=conn, params=(sourceID,))
+
+    return df
+    
+def getStructureFromSyn(conn, syn):
+    """
+    Return the smiles for the given synonym.
+    Arguments:
+          - conn: psycopg2 connection to the database.
+          - syn: Synonym.
+    """
+    curs = conn.cursor()
+    cmd = "SELECT smiles \
+            FROM substance AS subs \
+            INNER JOIN synonym AS syn ON subs.id = syn.subsid\
+            WHERE name = %s;"
+    curs.execute(cmd, (syn,))
+    smiles = curs.fetchone()
+    conn.commit()
+
+    if smiles is not None:
+        smiles = smiles[0]
+
+    return smiles
+
+def getSubstancesFromSynonyms(conn, synList):
+    """
+    Retruns a pandas dataframe with all substances for the given source.
+    The dataframe contains:
+        - synonym
+        - exernalid
+        - smiles
+        - inchi
+        - inchikey
+        - link 
+    Arguments:
+        - conn: psycopg2 connection to the database.
+        - synList: List or tuple with synonyms.
+    """
+    synList = tuple(synList)
+    cmd = "SELECT DISTINCT ON (synonym, externalid) \
+            name AS synonym, externalid, smiles, inchi, inchikey, link \
+            FROM public.synonym AS syn \
+            LEFT JOIN public.substance AS subs ON syn.subsid = subs.id \
+            WHERE name in %s"
+
+    df = pd.read_sql(cmd, con=conn, params= (synList,))
+
+    return df
+
+def getAnnotationsFromSource(conn, sourceID= None, sourceName= None, version= None):
+    """
+    Retruns a pandas dataframe with all annotations for the given source.
+    Arguments:
+        - conn: psycopg2 connection to the database.
+        - sourceID: Optional. Internal source ID if avaialble. Otherwise, it will be retrieved    from he source name and version.
+        - sourceName: Source name.
+        - version: Optional. Source's version (default: None). If None, 
+          the last version will be used.
+        without a chemical structure.
+    """
+    if sourceID is None:
+        sourceID = getSourceID(conn, sourceName, version)
+
+    if includeempty:
+        cmd = "SELECT externalid, smiles, inchi, inchikey, subs.link \
+                FROM public.substance AS subs \
+                INNER JOIN public.source AS s ON s.id = subs.sourceid \
+                WHERE s.id = %s"
+    else:
+        cmd = "SELECT externalid, smiles, inchi, inchikey, subs.link \
+                FROM public.substance AS subs \
+                INNER JOIN public.source AS s ON s.id = subs.sourceid \
+                WHERE s.id = %s AND smiles IS NOT NULL"
+
+    df = pd.read_sql(cmd, con=conn)
+
+    return df
+
+def getAnnotationsForSynonyms(conn, synList):
+    """
+    Retruns a pandas dataframe with all annotations for the given source.
+    Arguments:
+        - conn: psycopg2 connection to the database.
+        - synList: List or tuple with synonyms.
+    """
+    synList = tuple(synList)
+
+    cmd = "SELECT name, annotation, category, general, sa.type \
+            FROM public.synonym AS syn \
+            LEFT JOIN public.substance AS subs ON syn.subsid = subs.id \
+	        LEFT JOIN public.subs_ann AS sa ON subs.id = sa.subsid \
+            LEFT JOIN public.annotation AS ann ON sa.annid = ann.id \
+            WHERE name IN %s"
+
+    df = pd.read_sql(cmd, con=conn, params= (synList,))
+
+    return df
+
+def getAnnotationsForIDS(conn, idsList):
+    """
+    Retruns a pandas dataframe with all annotations for the given source.
+    Arguments:
+        - conn: psycopg2 connection to the database.
+        - idsList: List or tuple with substances IDs.
+    """
+    idsList = tuple(idsList)
+
+
+
+def deleteSource(conn, sourceID):
+    """
+    Deletes and source and all its associated substances.
+    """
+    curs = conn.cursor()
+    cmd = "DELETE FROM public.source WHERE id=%s"
+    curs.execute(cmd, (sourceID,))
+    conn.commit()
