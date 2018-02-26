87d768027c145849594f8023cdbfa57e61b41745
bet-gregori <elisabet.gregori@gmail.com>
2018-02-26 15:22:51 +0100

    Now annD.pkl is available in compoundDB

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup
 
-setup(name='compoundDB',
+setup(name='phidb',
       version='0.1',
-      description='Tools for creating, querying and managing the compounds PostgreSQL database',
+      description='Tools for creating, querying and managing the compounds and the annotations PostgreSQL database',
       url='http://github.com/phi-grib/compoundDB',
       author='Elisabet Gregori',
       author_email='elisabet.gregori@upf.edu',
-      license='Apache License, Version 2.0',
-      packages=['compoundDB'],
+      license='GNU',
+      packages=['compoundDB', 'annotationDB'],
+      package_data={
+        'annotationDB': ['data/*'],
+        'compoundDB': ['data/*'],
+        },
       zip_safe=False)
